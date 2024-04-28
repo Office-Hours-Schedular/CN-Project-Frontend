35d09ee0f8ad3bb0e528739077465736590b250a
Shravya Chepa <shravyachepa@shravya.local>
2024-04-27 18:34:37 -0700

    feat: add logo and app name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { LogoStyle, HeaderStyle } from "./Header.styled";
+import { HeaderStyle } from "./Header.styled";
+import AppLogo from "../../assets/images/logo.png";
 
 const Header = () => {
   return (
     <div>
       <HeaderStyle>
-        <LogoStyle>
-          <a href="https://www.sdsu.edu/">
-            <img
-              src="https://ou-resources.sdsu.edu/images/_sdsu/logo-2022/logo.png"
-              alt="San Diego State University"
-              height="40"
-              width="176"
-            />
-          </a>
-        </LogoStyle>
+        <a href="https://www.sdsu.edu/">
+          <img
+            src="https://ou-resources.sdsu.edu/images/_sdsu/logo-2022/logo.png"
+            alt="San Diego State University"
+            height="40"
+            width="176"
+          />
+        </a>
+
+        <img src={AppLogo} alt="logo" height="50" />
       </HeaderStyle>
     </div>
   );
